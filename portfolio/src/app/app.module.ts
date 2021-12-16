69c505968ba834ae886b34f6575ffb7dad7e6649
miguelopez13 <mlopari216@g.educaand.es>
2021-12-16 18:53:49 +0100

    building project structure

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/portfolio/components/header/header.component';
+import { CvComponent } from './modules/portfolio/components/cv/cv.component';
+import { NavComponent } from './modules/portfolio/components/nav/nav.component';
+import { PersonalInformationComponent } from './modules/portfolio/pages/personal-information/personal-information.component';
+import { PortfolioComponent } from './modules/portfolio/pages/portfolio/portfolio.component';
+import { LanguagesComponent } from './modules/portfolio/pages/languages/languages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CvComponent,
+    NavComponent,
+    PersonalInformationComponent,
+    PortfolioComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule,
