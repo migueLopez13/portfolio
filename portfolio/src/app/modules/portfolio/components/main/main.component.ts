20b742789a633d9397bdbdf3df0c4779f51cf811
miguelopez13 <mlopari216@g.educaand.es>
2021-12-23 14:05:33 +0100

    build canvas and main page

--- portfolio/src/app/modules/portfolio/components/main/main.component.ts
+++ portfolio/src/app/modules/portfolio/components/main/main.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements AfterViewInit {
+  @ViewChild('canvas') canvas!: ElementRef;
+  ctx!: CanvasRenderingContext2D;
+  rect!: DOMRect;
+  x = 0;
+  y = 0;
+  painting = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.ctx.canvas.width = window.innerWidth;
+    this.ctx.canvas.height = window.innerHeight;
+    this.rect = this.canvas.nativeElement.getBoundingClientRect();
+    this.listenerMouseEnter();
+    this.listenerMouseMove();
+    this.listenerMouseOut();
   }
 
+  public listenerMouseEnter() {
+    this.canvas.nativeElement.addEventListener(
+      'mouseenter',
+      (e: MouseEvent) => {
+        this.x = e.clientX - this.rect.left;
+        this.y = e.clientY - this.rect.top;
+        this.painting = true;
+      }
+    );
+  }
+
+  public listenerMouseMove() {
+    this.canvas.nativeElement.addEventListener('mousemove', (e: MouseEvent) => {
+      if (this.painting) {
+        let xf = e.clientX - this.rect.left;
+        let yf = e.clientY - this.rect.top;
+
+        this.paint(this.x, this.y, xf, yf);
+
+        this.x = xf;
+        this.y = yf;
+      }
+    });
+  }
+
+  public listenerMouseOut() {
+    this.canvas.nativeElement.addEventListener('mouseout', (e: MouseEvent) => {
+      this.painting = false;
+      this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    });
+  }
+
+  public paint(xi: number, yi: number, xf: number, yf: number) {
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = '#0bc9cd';
+    this.ctx.lineWidth = 1;
+    this.ctx.moveTo(xi, yi);
+    this.ctx.lineTo(xf, yf);
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
 }
