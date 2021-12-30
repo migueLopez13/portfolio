234e314eaadfa2ecba41b0d0bed9c0c81becf197
miguelopez13 <mlopari216@g.educaand.es>
2021-12-30 12:12:48 +0100

    build nav buttons & router navigation function

--- portfolio/src/app/shared/components/nav/nav.component.ts
+++ portfolio/src/app/shared/components/nav/nav.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  constructor(private readonly router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public navigate(where: string) {
+    this.router.navigate([`/${where}`]);
   }
-
 }
